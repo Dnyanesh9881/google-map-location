c80ac12f166b2aa055f78451059a0e96c4ba2dd6
Dnyaneshwar Suryawanshi <suryawanshidnyaneshwar59@gmail.com>
2023-12-03 18:25:32 +0530

    final final

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let button = document.getElementById("fetch-btn");
 button.addEventListener("click", () => {
   const API_KEY = "a059d01100076699ed971258da736401";
-
+  const map = document.getElementById("map");
   getLocation();
   function getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(showPosition,showError);
     } else {
-      x.innerHTML = "Geolocation is not supported by this browser.";
+    //   map.innerText = "Geolocation is not supported by this browser.";
+    alert("Allow Location In Your Browser")
     }
   }
 
   function showPosition(position) {
-    const map = document.getElementById("map");
+    
     map.innerHTML = `<iframe src="https://maps.google.com/maps?q=${position.coords.latitude}, ${position.coords.longitude}&z=15&output=embed" frameborder="0" style="border:0" class="location-on-map"></iframe>`;
 
     const latitude = document.querySelector(".latitude");
@@ -23,12 +24,15 @@ button.addEventListener("click", () => {
     const weather = document.querySelector(".weather-data");
 
     async function fetchWheather() {
-      let response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}&lang=en&units=metric`
-      );
-      let data1 = await response.json();
-      weatherAdd(data1);
-      console.log(data1);
+        try{
+            let response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}&lang=en&units=metric`
+              );
+              let data1 = await response.json();
+              weatherAdd(data1);
+        }catch{
+              weather.innerText="check your API"
+        }
     }
     function weatherAdd(data) {
       weather.innerHTML = ` <div class="Weather-info">Location: ${data.name}</div>
@@ -41,6 +45,22 @@ button.addEventListener("click", () => {
            <div class="Weather-info">Feels Like: ${data.main.feels_like} °C</div>`;
     }
     fetchWheather();
+  }
+  function showError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        map.innerHTML = "User denied the request for Geolocation."
+        break;
+      case error.POSITION_UNAVAILABLE:
+        map.innerHTML = "Location information is unavailable."
+        break;
+      case error.TIMEOUT:
+        map.innerHTML = "The request to get user location timed out."
+        break;
+      case error.UNKNOWN_ERROR:
+        map.innerHTML = "An unknown error occurred."
+        break;
+    }
   }
   let container1 = document.querySelector(".container");
   container1.classList.add("hide");
